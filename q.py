d770a3a208804266e5afeea507386aa45a6a13c0
Nipun Batra <nipunbatra0@gmail.com>
2022-12-14 17:39:29 +0530

    added questions

--- q.py
+++ q.py
@@ -1,45 +1,58 @@
-import random
-import yaml
-import sys
 import os.path
+import random
+import sys
+
+import yaml
 from yaml import CLoader as Loader
 
-random.seed(13122022)
+random.seed(15122022)
+
 
 def randomize(paper):
     random.shuffle(paper)
     for q in paper:
-        if 'options' in q:
-            perm = list(range(len(q['options'])))
+        if "options" in q:
+            perm = list(range(len(q["options"])))
             random.shuffle(perm)
-            q['perm'] = perm
+            q["perm"] = perm
+
 
 paper = list(yaml.load_all(open(sys.argv[1], "r"), Loader=Loader))
 
+
 def preamble(outfile):
-    outfile.write(r"""
+    outfile.write(
+        r"""
     \documentclass[twocolumn,10pt,addpoints]{exam}
     \usepackage[margin=0.5cm]{geometry}
     \usepackage{listings}
     \usepackage{fontspec}
     \usepackage{comment}
+    \usepackage{savetrees}
 
-    \renewcommand{\arraystretch}{1.7}
+    \renewcommand{\arraystretch}{1.6}
     \usepackage{setspace}
 
     \begin{document}
-    """)
+    """
+    )
+
 
 def postamble(outfile):
-    outfile.write(r"""
+    outfile.write(
+        r"""
     \end{document}
-    """)
+    """
+    )
+
 
 def writeout(paper, key):
     outfile = open(f"{os.path.splitext(sys.argv[1])[0]}{key}.tex", "w")
     preamble(outfile)
     outfile.write(f"\\framebox{{\\huge\\bf {key}}}\\\\")
-    outfile.write(r"""
+    outfile.write(r"\framebox{\numpoints\ points.}")
+    outfile.write(
+        r"""
     \begin{tabular}{ll}
     Full Name&\underline{\phantom{xxxxxxxxxxxxxxx}}\\
     Section \& Subsection &\underline{\phantom{xxxxxxxxxxxxxxx}}\\
@@ -47,28 +60,31 @@ def writeout(paper, key):
     \end{tabular}
 
     \lstset{language=Python,basicstyle=\ttfamily}
-    """)
+    """
+    )
 
-    outfile.write(r"\framebox{\numpoints\ points.}")
     outfile.write(r"\begin{questions}")
     for q in paper:
         outfile.write(f"\\question[{q['marks']}] {q['description']}\n")
-        if 'options' in q:
+        if "options" in q:
             outfile.write(r"\begin{oneparchoices}")
-            for o in q['perm']:
+            for o in q["perm"]:
                 outfile.write(f"\\choice {q['options'][o]}")
             outfile.write(r"\end{oneparchoices}")
     outfile.write(r"\end{questions}")
     postamble(outfile)
 
+
 keyfile = open("keyfile.txt", "w")
+
 
 def write_key(paper):
     for q in paper:
-        if 'options' in q:
+        if "options" in q:
             keyfile.write(f"{'ABCDEFGH'[q['perm'].index(0)]}")
 
-for k in "ABCDEFGHIJKLMN":
+
+for k in "ABCDEFGHIJKLMN"[:2]:
     randomize(paper)
     writeout(paper, k)
     keyfile.write(f"{k}: ")
